9f05379136cf02ef1a9b5d3132916c468ca802d1
leonardopellegrini-github <leonardo.giulio.pellegrini@gmail.com>
2022-06-30 15:59:15 +0200

    seeder fatto

---
+++ database/seeds/ComicsTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ComicsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $comic = config('comics');
+
+        dump($comic);
+    }
+}
